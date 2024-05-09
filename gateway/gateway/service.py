a0e59d54241c61eccecae54d5c79a56d1b9cad42
Junhui Liao <ljchemistry0944@gmail.com>
2024-05-09 15:42:25 +0800

    feat:gateway service
    Add 2 http api in gateway serivce.
    - get counter
    - create counter

---
+++ gateway/gateway/service.py
@@ -0,0 +1,38 @@
+import json
+
+from marshmallow import ValidationError
+from nameko import config
+from nameko.exceptions import BadRequest
+from nameko.rpc import RpcProxy
+from werkzeug import Response
+
+from gateway.entrypoints import http
+from gateway.exceptions import CounterNotFound
+
+class GatewayService:
+    """
+    Service acts as a gateway to other services over http.
+    """
+    name = 'gateway'
+
+    region_counter_rpc = RpcRpoxy('region_counter')
+
+    @http(
+            "GET", "/counters/<string:counter_id>",
+            expected_exceptions = CounterNotFound
+    )
+    def get_counter(self):
+        """
+        Gets counter
+        """
+        pass
+
+    @http(
+            "POST", "/counters",
+            expected_exceptions = (ValidationError, BadRequest)
+    )
+    def create_counter(self):
+        """
+        Create a new region counter
+        """
+        pass
